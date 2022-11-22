d86fff4a43f7ccbbe3a906ab77a6991449ee03c2
italosll <italomsilva.if@gmail.com>
2022-11-22 16:30:02 -0300

    🔍 feat: improve SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,6 +36,7 @@ export default function Home() {
     <>
       <Head>
         <title>Portifólio - Ítalo Moreira Silva</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="title" content={title}/>
         <meta name="description" content={description}/>
 
