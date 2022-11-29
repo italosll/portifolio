d0f25f53e7777c87dbb6880cc2da5d228d420696
italosll <italomsilva.if@gmail.com>
2022-11-29 13:53:04 -0300

    ✨ feat: add download cv button

--- components/SectionHome.tsx
+++ components/SectionHome.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import styled from 'styled-components';
 import background from "../assets/backgrounds/background-home.jpg";
 import { devices } from '../styles/breakingpoints';
@@ -28,6 +29,16 @@ export function SectionHome( ):JSX.Element{
             pedacinho</a> na internet,
             Sinta se em casa :)
         </About>
+        <a href="/cv-italo-moreira-silva.pdf" download> 
+
+          <button type="button"> 
+            <Image
+              alt="icone de baixar documento"
+              width="15" 
+              height="18" 
+              src="/icons/icon-cv-download.svg" />
+              Baixar Curriculo</button>
+        </a>
       </ContentSection>
     </BackgroundContainer>    
  
@@ -47,6 +58,50 @@ const ContentSection = styled.section`
         overflow: visible;
     }
 
+    & div ~ a {
+        margin-top: 20px;
+        margin-left: auto;
+        margin-right: auto;
+   
+        width: 335px;
+        max-width: 100%;
+        
+        @media ${devices.laptop} {
+            width: 100%;
+            max-width: 100%;
+        }
+    }
+    
+    & button {
+        transition-duration: 0.2s;
+        background: #4400ff48;
+        border: 1px solid #2C01A6;
+        font-size: 1rem;
+        display: flex;
+        gap: 10px;
+        padding: 10px;
+        border-radius: 5px;
+        width: 100%;
+
+
+        :hover{
+            background: #2C01A6;
+            cursor: pointer;
+            transform: scale(1.05);
+        }
+
+        :active{
+            background: #21007b;
+            border: 1px solid #ffffff;    
+        }
+
+        @media ${devices.laptop} {
+            width: fit-content;
+
+        }
+    }
+
+ 
 
 `
 
@@ -89,6 +144,7 @@ const Description = styled.div`
     margin-top: 18px;
     margin-left: auto;
     margin-right: auto;
+    
  
     @media ${devices.laptop} {
         font-size:3rem;
